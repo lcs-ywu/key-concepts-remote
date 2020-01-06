237ea0cd9119e8d16443ea9e3c047c6ffb471d44
Russell Gordon <rgordon@lcs.on.ca>
2020-01-06 15:03:48 -0500

    Fixed location of playground.

---
+++ KeyConcepts.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/*:
+ [Next](@next)
+ # Introduction
+ Most of the pages in this review package present visual output via the Live View.
+ If you do not see any output, remember to turn on Live View.
+ 
+ ![liveview](liveview.png "Live View")
+ */
